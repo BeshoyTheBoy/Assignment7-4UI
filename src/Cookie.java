f51c89b6da0e3d2d37da2a1d2e97bec1c7f89426
User <User@192.168.2.8>
2017-12-15 17:14:02 -0500

    Cant get price of cookie to show up... weird

--- src/Cookie.java
+++ src/Cookie.java
@@ -16,13 +16,39 @@ public class Cookie extends DessertItem{
     
     public Cookie(String name, int number, int pricePer12){
         super(name);
+        
+        this.number = number;
+        this.pricePer12 = pricePer12;
     }
 
     
-//    @Override
-//    public String toString(){
-//        
-//    }
+    @Override
+    public String toString(){
+        //make a new string to add to
+        String space = "";
+        
+        //convert cost to dollars and cents
+        String costDollar = DessertShoppe.cents2dollarsAndCents(getCost());
+        
+        //first string with number of cookies and price per dozen
+        String info = number + " @ $" + DessertShoppe.cents2dollarsAndCents(pricePer12) + " /dz";
+
+        space += info + "\n";
+
+        //add name of item to it
+        String name = super.getName();
+        space += name;
+        
+        //find left over number of spaces
+        for(int i=0; i < DessertShoppe.RECEIPT_WIDTH - (costDollar.length() + name.length()); i++){
+            space += " ";
+        }
+        
+        //add the cost after
+        space += costDollar;
+        
+        return space;
+    }
 
     @Override
     public int getCost() {
