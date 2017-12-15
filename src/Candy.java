d7a218898cb6805ef5d79c3125f28c7d338db32c
User <User@192.168.2.8>
2017-12-15 18:25:48 -0500

    Some prices of Candy is wrong????

--- src/Candy.java
+++ src/Candy.java
@@ -16,14 +16,40 @@ public class Candy extends DessertItem{
     
     public Candy(String name, double weight, int pricePerLbs){
         super(name);
+        
+        this.weight = weight;
+        this.pricePerLbs = pricePerLbs;
     }
 
  
     
-//    @Override
-//    public String toString(){
-//       
-//    }
+    @Override
+    public String toString(){
+       //make a new string to add to
+        String space = "";
+        
+        //convert cost to dollars and cents
+        String costDollar = DessertShoppe.cents2dollarsAndCents(getCost());
+        
+        //first string with number of cookies and price per dozen
+        String info = weight + " lbs. @ $" + DessertShoppe.cents2dollarsAndCents(pricePerLbs) + " /lb.";
+
+        space += info + "\n";
+
+        //add name of item to it
+        String name = super.getName();
+        space += name;
+        
+        //find left over number of spaces
+        for(int i=0; i < DessertShoppe.RECEIPT_WIDTH - (costDollar.length() + name.length()); i++){
+            space += " ";
+        }
+        
+        //add the cost after
+        space += costDollar;
+        
+        return space;
+    }
     
     //calculate cost of candy
     @Override
